9f06e4f2597891697cc83a992915f04ef0950e13
Cthulhux <git@tuxproject.de>
2013-08-30 00:54:24 +0200

    bugfixes

--- .gnus.el
+++ .gnus.el
@@ -1,7 +1,5 @@
 (load-file "~/.gnus.userdata.el") ; don't expose my mail address to github :)
 
-(setq gnus-select-method '(nntp "news.piratenpartei.de" (nntp-connection-timeout 10))) ;; 10 seconds
-
 (setq gnus-article-decode-mime-words t
       gnus-article-decode-charset 1
       gnus-agent t
@@ -11,48 +9,46 @@
       message-directory "~/.emacs.d/Mail/"
 
       gnus-agent-directory        (concat gnus-directory "agent/")
-      nndraft-directory           (concat message-directory "drafts/")
       gnus-cache-directory        (concat gnus-directory "cache/")
       gnus-cache-active-file      (concat gnus-directory "cache/active") ; no slash! this is a file, not a directory!
       gnus-article-save-directory (concat gnus-directory "save/")
       gnus-kill-files-directory   (concat gnus-directory "killfiles/")
+
+      nndraft-directory           (concat message-directory "drafts/")
       nnfolder-directory          (concat message-directory "archive/")
 
-      gnus-mime-view-all-parts t	; View all the MIME parts in current
+      gnus-mime-view-all-parts t    ; View all the MIME parts in current
 
       gnus-always-read-dribble-file 1 ; always read auto-save file
-      gnus-group-default-list-level 3 ; list all subscribed groups  ; TODO, this does nothing?!
+
+      gnus-thread-sort-functions '(gnus-thread-sort-by-number (not gnus-thread-sort-by-date)) ; inverted sorting: newest threads on top
 
       gnus-use-cache t
 
-      gnus-ignored-mime-types '("text/x-vcard")
-      gnus-buttonized-mime-types '("multipart/encrypted" "multipart/signed")
-      gnus-unbuttonized-mime-types '("text/plain")
-
-      gnus-treat-buttonize t		   ; Add buttons
-      gnus-treat-buttonize-head 'head	   ; Add buttons to the head
-      gnus-treat-emphasize t		   ; Emphasize text
-      gnus-treat-display-smileys t ; Use Smilies
-      gnus-treat-strip-cr 'last		   ; Remove carriage returns
-      gnus-treat-hide-headers 'head	   ; Hide headers
+      gnus-treat-buttonize t           ; Add buttons
+      gnus-treat-buttonize-head 'head  ; Add buttons to the head
+      gnus-treat-emphasize t           ; Emphasize text
+      gnus-treat-display-smileys t     ; Use Smilies
+      gnus-treat-strip-cr 'last        ; Remove carriage returns
+      gnus-treat-hide-headers 'head    ; Hide headers
 
       gnus-boring-article-headers '(empty followup-to newsgroups many-to reply-to)
       gnus-treat-hide-boring-headers 'head ; -Hide boring headers
+      gnus-fetch-old-headers 'some         ; prevent teared threads by loading older but read postings
 
-      gnus-outgoing-message-group nil) ; Don't save outgoing messages to a separate group
+      gnutls-log-level 0 ; don't annoy me with GnuTLS messages. I don't need that for NNTP.
+
+      gnus-message-archive-group  nil  ; Don't use archiving
+      gnus-outgoing-message-group nil) ; Don't save outgoing messages
 
 (defun my-message-mode-setup ()
   (setq fill-column 72)
   (turn-on-auto-fill))
 (add-hook 'message-mode-hook 'my-message-mode-setup)
-
-(setq gnus-thread-sort-functions '(gnus-thread-sort-by-number (not gnus-thread-sort-by-date))) ; newest stuff on top
  
-(setq gnus-fetch-old-headers 'some) ; prevent teared threads by loading older but read postings
-
-; "<name> writes" is a bit boring.
+; "<name> writes:" is a bit boring.
 (setq message-citation-line-function 'message-insert-formatted-citation-line)
-(setq message-citation-line-format "%f schrob am %d. %b. %Y um %R Uhr dies:\n")
+(setq message-citation-line-format "%N schrob am %d. %b. %Y um %R Uhr dies:\n") ; %N = (real name, else mail address)
 
 (gnus-demon-add-handler 'gnus-demon-scan-news 2 t) ; grab new news every 2 minutes
 
@@ -69,4 +65,7 @@
 (setq gnus-visible-headers "^From:\\|^Subject:\\|To:\\|^Cc:\\|^Date:\\|^Newsgroups:\\|^X-Newsreader:\\|^X-Mailer:"
       gnus-sorted-header-list gnus-visible-headers)
 
-(setq gnutls-log-level 0) ; don't annoy me with GnuTLS messages. I don't need that for NNTP.
+; list all groups, not only the unread ones, in case I want to post something to them...
+;(add-hook 'gnus-started-hook 'gnus-group-list-all-groups)
+;(add-hook 'gnus-summary-exit-hook 'gnus-group-list-all-groups)
+(setq gnus-permanently-visible-groups ".*") ; more elegant method
